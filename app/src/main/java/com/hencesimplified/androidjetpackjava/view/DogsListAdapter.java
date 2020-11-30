1939e10fa156b1f48d74c8cbc4a5176f55e9b9ef
praveen05git <praveenmail@protonmail.com>
2020-11-30 16:58:09 +0530

    Started Data binding

--- app/src/main/java/com/hencesimplified/androidjetpackjava/view/DogsListAdapter.java
+++ app/src/main/java/com/hencesimplified/androidjetpackjava/view/DogsListAdapter.java
@@ -1,7 +1,6 @@
 package com.hencesimplified.androidjetpackjava.view;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
